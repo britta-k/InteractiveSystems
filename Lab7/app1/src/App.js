3d7e2bd1a47dd12f0605b01c2458400096394a98
britta-k <karibritta22@gmail.com>
2024-05-02 22:16:34 +0300

    movies api app continued in class

--- Lab7/app1/src/App.js
+++ Lab7/app1/src/App.js
@@ -4,9 +4,11 @@ import APIKey from './config';
 import Movie from './Components/Movie';
 
 const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=';
+const ImageURL = 'https://image.tmdb.org/t/p/w500/';
 
 function App() {
   const [movieResults, setMovieResults] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
   //const movieResults = [1,2,3];
 
   useEffect( ()=> {
@@ -19,12 +21,39 @@ function App() {
       )
   }
   ,[])
+  
+
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm)
+    {
+      fetch(SearchURL + APIKey + "&query=" + searchTerm)
+     .then( (resp) => resp.json() )
+     .then( (data) => {
+        console.log(data);
+        setMovieResults(data.results);
+     })
+    
+    }
+  }
+
+  const handleOnChange = (e) => {
+    console.log(e.target.value)
+    setSearchTerm(e.target.value)
+  }
 
   return (
     <>
-    {movieResults.map( (element,index) => (<Movie key={index}/>)
-
-    )}
+      <header>
+        <form onSubmit={handleOnSubmit}>
+          <input className='search' type='search' placeholder='Search...' value={searchTerm} onChange={handleOnChange}></input>
+        </form>
+      </header>
+      <div className='movie-container'>
+      {movieResults.map( (element) => (<Movie key={element.id} {...element}/>))}
+      </div>
     </>
   );
 }
--- Lab7/app1/src/Components/Movie.js
+++ Lab7/app1/src/Components/Movie.js
@@ -1,9 +1,22 @@
 import React from "react";
+import "./Movie.css"
 
-function Movie()
+const ImageURL = 'https://image.tmdb.org/t/p/w500/';
+
+function Movie({poster_path,title,overview,vote_average})
 {
     return(
-        <div>This is a movie component</div>
+        <div className="movie">
+            <img srs={ImageURL + poster_path} alt={title}></img>
+            <div className="movie-info">
+                <h3>{title}</h3>
+                <span>{vote_average}</span>
+            </div>
+            <div className="movie-overview">
+                <h2>Overview:</h2>
+                <p>{overview}</p>
+            </div>
+        </div>
     );
 }
 
