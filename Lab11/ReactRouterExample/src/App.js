0cf1aeb8e8c1bc43d68c0cd23b05e9f47660fb4a
britta-k <karibritta22@gmail.com>
2024-05-09 19:57:59 +0300

    Progress in class

--- Lab11/ReactRouterExample/src/App.js
+++ Lab11/ReactRouterExample/src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+//import {home} from './components/Home';
 import './App.css';
+import {NotFound} from './components/NotFound';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <h1>Hello World</h1>
+      <div>Hello</div>
+      <Routes>
+        <Route path="/" element={<home/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
